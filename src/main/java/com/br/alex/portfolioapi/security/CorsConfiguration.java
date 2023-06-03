9a3f0d9dd37f1b825bdddcf268d1ad9cc35daa8f
Alex  Fogazzi <alexrfogazzi@gmail.com>
2023-06-02 22:36:41 -0300

    refactor: restringe acesso a api configurando url do front no cors config

--- src/main/java/com/br/alex/portfolioapi/security/CorsConfiguration.java
+++ src/main/java/com/br/alex/portfolioapi/security/CorsConfiguration.java
@@ -1,5 +1,6 @@
 package com.br.alex.portfolioapi.security;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +8,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfiguration  implements WebMvcConfigurer {
 
+    private final String urlFront = Dotenv.load().get("URL_FRONT");
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins(this.urlFront)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 }
